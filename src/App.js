8fb1642b41d21d89a2f655c8f7e20a14b61e12f7
Shirley Lei <syl573@nyu.edu>
2018-12-10 19:19:25 +0800

    added styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from "./Components/login";
 import Questionsqueue from "./Components/queue";
 import firebase from 'firebase';
+
 import {firebase_config} from './firebase_config.js';
 firebase.initializeApp(firebase_config);
 const database = firebase.database();
@@ -39,28 +40,18 @@ class App extends Component {
   }
 
   pushToDB(path, data){
-    // Level 1 - read this if you are coming here from Levels/LevelONE.js
-    // 
-    // this is where want to create a simple function to push data to
-    // our Firebase project. To test the function, we use a dummy 
-    // button in this very component (prepared below)
-
-
-    // we can learn how to push to the database from here:
-    // https://firebase.google.com/docs/database/web/lists-of-data
     let reference = database.ref(path);
     var newPostRef= reference.push();
     newPostRef.set(data);
   }
 
   componentDidMount() {
-    let reference = database.ref("data");
-    reference.on("child_added", (newData) =>{
-    
-      console.log(this.state.data);
+    database.ref("Question Directory").on("child_added", (newObject) =>{
+
+      console.log(this.state.questions);
       this.setState({
-        data: this.state.data.concat( [ newData.val()] )
-      })
+        questions: this.state.questions.concat( [newObject.val()] )
+      });
     })
   }
 
@@ -72,13 +63,13 @@ class App extends Component {
     let updateQuestions = this.state.questions;
     let x = {
       text: newQuestion,
-      votes: 0
+      votes: 1
     }
     updateQuestions.push(x);
     this.setState({
       questions: updateQuestions
     })
-    this.pushToDB("Question Directory",newQuestion);
+    this.pushToDB("Question Directory",x);
   }
 
   submittingButton = () => this.addQuestion(this.state.value)
@@ -96,7 +87,6 @@ class App extends Component {
         numQuestions: 0
       });
     }
-
     this.setState({
       value: ""
     })
@@ -143,7 +133,6 @@ class App extends Component {
     this.setState({currentPage:"login"});
   }
 
-
   usedVote = () => {
     let totalVotes = this.state.numVotes;
     if (totalVotes > 0) {
@@ -163,38 +152,51 @@ class App extends Component {
     if (this.state.currentPage === "login") {
       partial =
       <div className="App">
-        <h1>Hello!</h1>
-        <h2>Feel free to leave a question, or vote on a question.</h2>
-        <p>{this.state.numVotes}/3 votes left!</p>
-        <p>{this.state.numQuestions}/3 questions left!</p>
-
-
-        <form onSubmit={this.handleSubmit }>
-          <div className="container">
-            <textarea disabled={this.state.numQuestions==0} className="box" label="Ask a question!" value={this.state.value} onChange={this.handleChange} /> <br />
-            <br/>
-          </div>
-          <button disabled={!this.state.value} type="submit" name="action">Submit</button>
-        </form>
+        <div className="input-section">
+          <h4>Leave a question for the class, or vote on an existing question.</h4>
+          <form onSubmit={this.handleSubmit }>
+            <div className="container">
+              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" disabled={this.state.numQuestions==0} placeholder="Type your question here..." value={this.state.value} onChange={this.handleChange} />
+            </div>
+            <button className="btn btn-outline-warning give-padding" disabled={!this.state.value} type="submit" name="action">Submit</button>
+          </form>
+          <small id="emailHelp" className="form-text text-muted">{this.state.numQuestions}/3 questions left  ·  {this.state.numVotes}/3 votes left</small>
+        </div>
 
        <div><Questionsqueue data={this.state.questions.sort(this.compareFunction).filter(this.filterFunction)} upvote={(i) => {this.bumpQuestion(i);this.usedVote();}} votes={this.state.numVotes}/></div>
      </div>
    } else if (this.state.currentPage === "main"){
      partial =
-     <div>
-      <h1>{"Hello, please login to your student or professor account below using your NetID."}</h1>
+     <div className="login_div">
+      <h3>{"Welcome to HiveMind."}</h3>
+      <h6>{"Please login to your student or professor account."}</h6>
+      <br></br>
+
       <form>
-        <input type="text" />
-        <br></br>
-        <input className="next_button" type="submit" value="Login" onClick={this.swapPage}/>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Username</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="NetID"></input>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+          <small id="emailHelp" className="form-text text-muted">Never share your password with anyone else.</small>
+        </div>
+        <button type="submit" className="btn btn-outline-warning give-padding" onClick={this.swapPage}>Login</button>
       </form>
+
     </div>
 
    }
-
     return (
       <div>
-        <h1>HiveMind</h1>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+          <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""></img>
+          HiveMind
+          </a>
+          <span className="navbar-text">For classroom success ©</span>
+        </nav>
         <div>{partial}</div>
       </div>
    );
--- src/Components/queue.js
+++ src/Components/queue.js
@@ -1,24 +1,34 @@
 import React, {Component} from 'react';
 class Questionsqueue extends Component {
-
-    listItems = () => {
-      let items = this.props.data.map((questionsData, i)=>{
-        let element = <li key={i}>
-          <p>{questionsData.text}</p>
-          <p>VOTES:  {questionsData.votes}</p>
-          <button disabled={this.props.votes==0 } onClick={ ()=>{this.props.upvote(i);} }>Up</button>
-        </li>;
-        return element;
+  componentDidUpdate(){
+    let listElement = this.refs.list;
+    let listItem = listElement.lastChild;
+    if(listItem!=null){
+      listElement.scrollTo({
+        top: listItem.offsetTop,
+        left: 0,
+        behavior: 'smooth'
       });
-      return(items);
     }
+  }
+  listItems = () => {
+    let items = this.props.data.map((questionsData, i)=>{
+      let element = <li key={i}>
+        <p>{questionsData.text}</p>
+        <p>VOTES:  {questionsData.votes}</p>
+        <button className="btn btn-outline-info" disabled={this.props.votes==0 } onClick={ ()=>{this.props.upvote(i);} }>UP</button>
+      </li>;
+      return element;
+    });
+    return(items);
+  }
 
-    render () {
-        return (
-          <ul>
-            {this.listItems()}
-          </ul>
-        );
-    }
+  render () {
+    return (
+      <ul ref="list">
+        {this.listItems()}
+      </ul>
+    );
+  }
 }
 export default Questionsqueue;
